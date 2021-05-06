12bd4862abb5eb67d235c520d8ab1ca7907c40ef
vinay <vinay1289@gmail.com>
2021-05-06 13:55:22 +0530

    code devepoed by vinay for amezon site

---
+++ src/main/java/amezon/App.java
@@ -0,0 +1,16 @@
+package amezon;
+
+/**
+ * Hello world!
+ *
+ */
+public class Array {
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+
+        for (int element: array) {
+            System.out.println(element);
+        }
+    }
+}}
